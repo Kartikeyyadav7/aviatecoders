30d445fd3ba4ea01ace3b32b79a4c0769cf48b17
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2021-07-26 15:41:34 +0530

    made all the images smaller in size and added reading-time for each for post and added all the posts

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -5,12 +5,14 @@ interface BlogCardProps {
 	title: string;
 	publishedOn: Date;
 	coverImage: string;
+	timeForReading: string;
 }
 
 const BlogCard: React.FC<BlogCardProps> = ({
 	title,
 	publishedOn,
 	coverImage,
+	timeForReading,
 }) => {
 	const date: Date = new Date(publishedOn);
 	return (
@@ -18,7 +20,7 @@ const BlogCard: React.FC<BlogCardProps> = ({
 			<div className="max-w-sm rounded-2xl overflow-hidden shadow-md hover:shadow-2xl transition duration-300 ease-in-out border-2 border-solid border-opacity-5 border-black">
 				<div className="px-6 py-4">
 					<div className="rounded-lg">
-						<Image width={500} height={300} src={coverImage} alt="hoisting" />
+						<Image width={500} height={300} src={coverImage} alt={title} />
 					</div>
 
 					<div className="font-bold text-xl mb-0 padding-top={1.25 rem}">
@@ -27,7 +29,7 @@ const BlogCard: React.FC<BlogCardProps> = ({
 				</div>
 				<div className="px-6 pt-2 pb-1">
 					<span className="inline-block bg-gray-200 rounded-full px-3  text-sm font-semibold text-gray-700 mr-2 mb-2">
-						7 minute read
+						{timeForReading}
 					</span>
 					<span className="inline-block bg-gray-200 rounded-full px-3  text-sm font-semibold text-gray-700 mr-2 mb-2">
 						{date.toDateString()}
