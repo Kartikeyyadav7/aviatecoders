e68a75ff3aa886ee5ddbcb92ee11ba0212cc4697
ritik <ritiksingh2615@gmail.com>
2021-08-09 23:31:49 +0530

    image resizing and added in mdx files

--- components/ContactUs.jsx
+++ components/ContactUs.jsx
@@ -35,7 +35,7 @@ export default function ContactUs() {
         Message
       </label>
       <textarea className=" no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
-      <p className="text-gray-600 text-xs italic">Re-size can be disabled by set by resize-none / resize-y / resize-x / resize</p>
+     
     </div>
   </div>
   <div className="md:flex md:items-center">
