432a12a209bdd47da69f636f3152efcff7e81744
Kartikey Yadav <kartikeyyadav.13579@gmail.com>
2021-08-12 22:05:42 +0530

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import { positions, Provider as ReactAlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			ga.pageview(url);
+		};
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		router.events.on("routeChangeComplete", handleRouteChange);
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
 	const options = {
 		timeout: 5000,
 		position: positions.BOTTOM_CENTER,
